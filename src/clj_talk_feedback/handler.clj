676e5513603383b4bb434ef171817054ad6e4bb2
Robert Rees <shudderfix@gmail.com>
2017-04-02 09:50:38 +0100

    Tries to introduce secure defaults if we are in production

--- src/clj_talk_feedback/handler.clj
+++ src/clj_talk_feedback/handler.clj
@@ -1,11 +1,18 @@
 (ns clj-talk-feedback.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults secure-site-defaults site-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
+
+(def production?
+    (= "production" (get (System/getenv) "APP_ENV")))
+
+(def development? (not production?))
+
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes
+      (if production? secure-site-defaults site-defaults)))
